6f642ef72059bde5fcff36d30ff7e835121fb68a
汪五海 <wangwuhai@xdf.cn>
2022-09-16 10:08:03 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls.Platform;
+
+namespace ElectronicClassBoard_Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, EventArgs e)
+        {
+            webView.Source = new UrlWebViewSource { Url = "http://class-board-demo.iwisesoft.com/welcome" };
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ElectronicClassBoard_Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
